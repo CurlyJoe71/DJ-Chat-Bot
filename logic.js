cd3ce90d04aaede4c4640f9715ba91889697d1e7
Curly Joe <curlyjoe71@gmail.com>
2018-09-19 20:35:35 -0700

    update

--- logic.js
+++ logic.js
@@ -8,8 +8,10 @@ var config = {
     messagingSenderId: "869856822775"
 };
 firebase.initializeApp(config);
-
 var ui = new firebaseui.auth.AuthUI(firebase.auth());
+
+// // The start method will wait until the DOM is loaded.
+// ui.start('#firebaseui-auth-container', uiConfig);
 
 var uiConfig = {
     callbacks: {
@@ -38,32 +40,66 @@ var uiConfig = {
     privacyPolicyUrl: '<your-privacy-policy-url>'
 };
 
-// The start method will wait until the DOM is loaded.
-ui.start('#firebaseui-auth-container', uiConfig);
-
-var connectedRef = database.ref(".info/connected");
+var database = firebase.database();
+var connectionsRef = database.ref('/connections');
+var connectedRef = database.ref('.info/connected');
 
 // When the client's connection state changes...
-connectedRef.on("value", function(snap) {
+connectedRef.on("value", function (snap) {
 
-  // If they are connected..
-  if (snap.val()) {
+    // If they are connected..
+    if (snap.val()) {
 
-    // Add user to the connections list.
-    var con = connectionsRef.push(true);
+        // Add user to the connections list.
+        var con = connectionsRef.push(true);
 
-    // Remove user from the connection list when they disconnect.
-    con.onDisconnect().remove();
-  }
+        // Remove user from the connection list when they disconnect.
+        con.onDisconnect().remove();
+    }
 });
+//lyrics variables
+var lyricsAPIKey = '9oV6enzrwLxHjuKbkfEuYuwqkDly9pSPHix8gcozDfSIcJ4i4kyoXSZT491L7QhC';
+var lyricsURL = 'https://orion.apiseeds.com/api/music/lyric/:artist/:track'
+var artistRequest = ''
+
 
 var spotifyAPIKey = '41f1e6b39f974a18bf48f26553dbb960';
 var youTubeAPIKey = 'AIzaSyDf7m2WR4gwTwBsU605wDNvuCydxt5GVU4';
 
-// // When first loaded or when the connections list changes...
-// connectionsRef.on("value", function(snap) {
 
-//   // Display the viewer count in the html.
-//   // The number of online users is the number of children in the connections list.
-//   $("#connected-viewers").text(snap.numChildren());
-// });
+function lyricsAJAX() {
+    $.ajax({
+        url: 'https://orion.apiseeds.com/api/music/lyric/smashing pumpkins/1979?apikey=9oV6enzrwLxHjuKbkfEuYuwqkDly9pSPHix8gcozDfSIcJ4i4kyoXSZT491L7QhC',
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+        var lyrics = response.result.track.text;
+        var artist = response.result.artist.name;
+        var title = response.result.track.name;
+        var lyricsDisplay = $('#lyricsDisplay');
+        var titleDisplay = $('#titleDisplay');
+        var artistDisplay = $('#artistDisplay');
+        artistDisplay.text(artist);
+        titleDisplay.text(title);
+        lyricsDisplay.text(lyrics);
+    });
+}
+
+// After the API loads, call a function to enable the search box.
+function handleAPILoaded() {
+    $('#search-button').attr('disabled', false);
+  }
+  
+  // Search for a specified string.
+  function search() {
+    var q = $('#query').val();
+    var request = gapi.client.youtube.search.list({
+      q: q,
+      part: 'snippet'
+    });
+  
+    request.execute(function(response) {
+      var str = JSON.stringify(response.result);
+      $('#search-container').html('<pre>' + str + '</pre>');
+    });
+  }
